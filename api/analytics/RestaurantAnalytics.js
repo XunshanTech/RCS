8bbf2c28d41b98178ea2a5c8aeee10bb26b21290
liyachuan <applesstt@gmail.com>
2015-03-24 20:42:34 +0800

    refine query for last 30 days data

--- api/analytics/RestaurantAnalytics.js
+++ api/analytics/RestaurantAnalytics.js
@@ -2,16 +2,39 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 // Connection URL
 var url = 'mongodb://localhost:27017/rcs_mongo';
-
+var last30Date = new Date(new Date() - 30 * 24 * 60 * 60 * 1000);
 module.exports = {
   data: function(restaurantId, cb) {
     // Use connect method to connect to the Server
     MongoClient.connect(url, function(err, db) {
       // Get the documents collection
       var collection = db.collection('request');
-      collection.group(['_id'], {
-        'Restaurant': new ObjectID(restaurantId)
-      }, {'count': 0}, "function (obj, prev) { prev.count++; }", function(err, results) {
+      collection.group(function(doc) {
+        var date = new Date(doc.createdAt);
+        var dateKey = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+        return {'date': dateKey};
+      }, {
+        'Restaurant': new ObjectID(restaurantId),
+        'createdAt': {
+          '$gt': last30Date
+        }
+      }, {'old': 0, 'new': 0, 'count': 0}, function (obj, prev) {
+        var types = {
+          call: [50, 50],
+          water: [100, 50],
+          order: [600, 0],
+          pay: [200, 50]
+        }
+        prev.count++;
+        if(typeof obj.Type !== 'undefined' && types[obj.Type]) {
+          prev.old += types[obj.Type][0];
+          prev.new += types[obj.Type][1];
+          prev.createdAt = obj.createdAt;
+        }
+      }, function(err, results) {
+        results.sort(function(a, b) {
+          return a.createdAt - b.createdAt;
+        })
         console.log(results);
         return cb(results);
       })
--- assets/js/data-chart.js
+++ assets/js/data-chart.js
@@ -6,7 +6,15 @@ var getData = function(restaurantId) {
     }
   });
 
-  var seriesOptions = [];
+  var seriesOptions = [{
+    name: '原始服务时间',
+    type: 'area',
+    data: []
+  }, {
+    name: '优化服务时间',
+    type: 'area',
+    data: []
+  }];
   // create the chart when all data is loaded
   var createChart = function () {
     $('#data-container').highcharts('StockChart', {
@@ -43,26 +51,30 @@ var getData = function(restaurantId) {
       },
 
       yAxis: {
+        min: 0,
         labels: {
           formatter: function () {
-            return this.value + '分钟';
+            var m = parseInt(this.value / 60);
+            var s = (this.value % 60);
+            var text = '';
+            if(m > 0) {
+              text += m + '分';
+            }
+            if(s > 0 || (s === 0 && m === 0)) {
+              text += s + '秒';
+            }
+            return text;
           }
         },
         plotLines: [{
           value: 0,
-          width: 2,
-          color: 'silver'
+          width: 2
         }]
       },
 
-      plotOptions: {
-        series: {
-        }
-      },
-
       tooltip: {
-        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}分钟</b><br/>',
-        valueDecimals: 2,
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}秒</b><br/>',
+        valueDecimals: 0,
         dateTimeLabelFormats: {
           day:"%Y年%b%e日,%A"
         }
@@ -81,74 +93,12 @@ var getData = function(restaurantId) {
     method: 'post',
     success: function(result) {
       console.log(result);
-      seriesOptions = [{
-        name: '原始服务时间',
-        data: [
-          /* Mar 2008 */
-          [1206316800000,29.17],
-          [1206403200000,29.14],
-          [1206489600000,28.56],
-          [1206576000000,28.05],
-          [1206662400000,27.91],
-          [1206921600000,28.38],
-          /* Apr 2008 */
-          [1207008000000,29.50],
-          [1207094400000,29.16],
-          [1207180800000,29.00],
-          [1207267200000,29.16],
-          [1207526400000,29.16],
-          [1207612800000,28.75],
-          [1207699200000,28.89],
-          [1207785600000,29.11],
-          [1207872000000,28.28],
-          [1208131200000,28.06],
-          [1208217600000,28.25],
-          [1208304000000,28.95],
-          [1208390400000,29.22],
-          [1208476800000,30.00],
-          [1208736000000,30.42],
-          [1208822400000,30.25],
-          [1208908800000,31.45],
-          [1208995200000,31.80],
-          [1209081600000,29.83],
-          [1209340800000,28.99],
-          [1209427200000,28.64],
-          [1209513600000,28.52]]
-      }, {
-        name: '优化服务时间',
-        data: [
-          /* Mar 2008 */
-          [1206316800000,19.93],
-          [1206403200000,20.14],
-          [1206489600000,20.72],
-          [1206576000000,20.04],
-          [1206662400000,20.43],
-          [1206921600000,20.50],
-          /* Apr 2008 */
-          [1207008000000,21.36],
-          [1207094400000,21.07],
-          [1207180800000,21.66],
-          [1207267200000,21.87],
-          [1207526400000,22.27],
-          [1207612800000,21.83],
-          [1207699200000,21.63],
-          [1207785600000,22.08],
-          [1207872000000,21.02],
-          [1208131200000,21.11],
-          [1208217600000,21.20],
-          [1208304000000,21.96],
-          [1208390400000,22.07],
-          [1208476800000,23.01],
-          [1208736000000,24.02],
-          [1208822400000,22.89],
-          [1208908800000,23.27],
-          [1208995200000,24.13],
-          [1209081600000,24.25],
-          [1209340800000,24.61],
-          [1209427200000,25.01],
-          [1209513600000,24.85]
-        ]
-      }]
+      for(var i = 0; i < result.length; i++) {
+        var ret = result[i];
+        var date = (new Date(ret.date)).getTime();
+        seriesOptions[0].data.push([date, ret.old]);
+        seriesOptions[1].data.push([date, ret.new]);
+      }
       window.setTimeout(createChart, 0);
     }
   })
